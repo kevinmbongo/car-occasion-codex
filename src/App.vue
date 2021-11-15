addea3ae050ced738c4fa7e112735efd30245401
kevinm <mbongo.kevin.pro@gmail.com>
2021-11-15 21:37:14 +0100

    feat(layout): creat app layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="app">Hello World</div>
+  <div id="app" class="grid">
+    <header class="bg-red-900 col-span-12">Header</header>
+    <nav class="bg-blue-800 col-span-4">Nav</nav>
+    <main class="bg-green-800 col-span-8">Main</main>
+  </div>
 </template>
 
 <script>
@@ -7,3 +11,9 @@ export default {
   name: 'App',
 }
 </script>
+
+<style>
+nav {
+  height: calc(100vh - 24px);
+}
+</style>
