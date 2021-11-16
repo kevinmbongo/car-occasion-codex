36d6784922d0c6792fba4fa0c3615dfac57db721
kevinm <mbongo.kevin.pro@gmail.com>
2021-11-16 16:51:19 +0100

    feat(layout): implement responsive for default layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="app" class="grid">
+  <div id="app" class="grid grid-cols-12">
     <header class="bg-red-900 col-span-12">Header</header>
-    <nav class="bg-blue-800 col-span-4">Nav</nav>
-    <main class="bg-green-800 col-span-8">Main</main>
+
+    <nav class="bg-blue-800 col-span-12 md:col-span-3">Nav</nav>
+
+    <main class="bg-green-800 col-span-12 md:col-span-9">Main</main>
   </div>
 </template>
 
@@ -11,9 +13,3 @@ export default {
   name: 'App',
 }
 </script>
-
-<style>
-nav {
-  height: calc(100vh - 24px);
-}
-</style>
